b2523bb9f5ac48230b38eac779448e18c41c44c6
Marvin Roger <nivramdu94@gmail.com>
2016-09-30 12:51:15 +0200

    Fix lang attribute, update project naming and change favicon

--- src/layout/AppBar.js
+++ src/layout/AppBar.js
@@ -4,7 +4,7 @@ import AppBar from 'material-ui/AppBar';
 class AppBarCustom extends Component {
   render() {
     return (
-        <AppBar title="UNION'TECH The Game" iconElementLeft={<span />}  />
+        <AppBar title="UNION'TECH: The Game" iconElementLeft={<span />}  />
     );
   }
 }
